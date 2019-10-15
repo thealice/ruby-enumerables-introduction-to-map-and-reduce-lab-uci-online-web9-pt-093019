61d3c4a06d44bc979dde2088b96754da1576f42f
Alice Freda <alicefreda@gmail.com>
2019-10-15 18:53:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,13 @@ def map_to_negativize(array)
   end
   negativized
 end
+
+def map_to_no_change(array)
+  unchanged = []
+  count = 0
+  while count < array.size do
+    unchanged << (array[count] * 1)
+    count += 1
+  end
+  unchanged
+end
